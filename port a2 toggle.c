2636c13ff6256b6adaf4b8274a9c22b9f7d9c450
YESVANTHKRISHNA.V <114081669+yesvanthkrishna16@users.noreply.github.com>
2024-06-16 20:18:40 +0530

    Create port a2 toggle.c

---
+++ port a2 toggle.c
@@ -0,0 +1,22 @@
+#include<stm32f10x.h>
+void delay_ms(unit16_t t) //initilizing a function
+void delay_ms(unit16_t t)//delay function for 72 MHz ARM  controller 
+{
+  volatile unsigned long l=0;
+  for(uint16_t i=0;i<=t;i++)
+    {
+      for(l=0;l<6000;l++)
+        {
+        }
+    }
+}
+int main()
+{
+  RCC->APB2ENR |=0xFC; //ENABLE THE CLOCK FOR THE GPIO PINS
+  GPIOA->CRL = 0x44444344 //PA0 TO PA7 AS OUTPUTS
+  while(1)
+    {
+       GPIOA-> ODR^=(1<<2);//toggle PORT A2
+      delay_ms(1000);//dely for 100ms
+    }
+}
